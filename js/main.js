613e78fec45a8c3a52a23c593b9d0e16df0afa89
Abdusamatov-Samandar <abdusamatovsamandardeveloper@gmail.com>
2024-03-04 05:58:57 +0500

    initial commit, done homework

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var avalibleMany = prompt("O'zingizdagi mavjud pul qiymatini kiritin! Namuna : 1500000.00",)
+
+var expenses = 9732761.1
+
+var residual = avalibleMany - expenses
+
+
+if(residual >= 0)
+    console.log("Oq yo'l, Alisher!")
+    // alert("Oq yo'l, Alisher!")
+else if(residual <= 0)
+    console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+    // alert("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+else
+    console.log("Alisher pul miqdorini kiritishda xatolik mavjud iltimos qayta urinib ko'ring! Namuna : 1500000.00")
+    // alert("Alisher pul miqdorini kiritishda xatolik mavjud iltimos qayta urinib ko'ring! Namuna : 1500000.00")
